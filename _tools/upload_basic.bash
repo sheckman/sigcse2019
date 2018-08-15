8b69355a6ebd255d39832a1059e2de6ea6c6b792
Josh Hug <hug@cs.berkeley.edu>
2018-08-15 09:10:21 -0700

    fixed philip easts name

---
+++ _tools/upload_basic.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function build {
+  rm -rf _site/
+  jekyll build --config _config.yaml,$1
+}
+
+
+function upload {
+  rsync -vr \
+    -e "ssh -p 7822 -i ~/.ssh/sigcse-key" _site/ \
+    sigcse@sigcse.hosting.acm.org:/home/sigcse/www/$1
+}
+
+# First, remove the old site.
+if [ -d _site ];
+then
+  echo "REMOVING THE OLD SITE."
+  rm -rf _site/
+fi
+
+if [ "$1" == "live" ] || [ "$1" == "all" ];
+then
+  build _config_live.yaml
+  upload sigcse2019/
+fi
+
+# If they want to upload the staging site, lets do that.
+if [ "$1" == "staging" ] || [ "$1" == "all" ];
+then
+  build _config_staging.yaml
+  upload sigcse2019/.staging
+fi
